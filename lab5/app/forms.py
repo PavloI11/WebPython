bd6ad8812ae915f7aac35ba2b46cf563986aab1e
PavloInv <147037242+PavloI11@users.noreply.github.com>
2024-01-04 15:07:02 +0200

    Add files via upload

---
+++ lab5/app/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class LoginForm(FlaskForm):
+    username = StringField("Ваш Username", validators=[DataRequired("Це поле обовʼязкове")])
+    password = PasswordField("Ваш пароль", validators=[
+                            DataRequired("Це поле обовʼязкове"),
+                            Length(min=4, max=10)
+                        ])
+    remember = BooleanField("Запамʼятати")
+    submit = SubmitField("Виконати")
+
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField("Новий пароль", validators=[
+                            DataRequired("Пароль повинен мати від 4 до 10 символів"),
+                            Length(min=4, max=10)
+                        ])
+    confirm_password = PasswordField("Підтвердити новий пароль", validators=[
+                            DataRequired("Пароль повинен мати від 4 до 10 символів"),
+                            Length(min=4, max=10)
+                        ])
+    submit = SubmitField("Виконати")
